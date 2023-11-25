669b2a8f4d5aae33f0ee05be82a26ecd14ed43bb
Faniahy RALISON <ralisonfaniahy@gmail.com>
2023-11-25 17:35:51 +0100

    Fix: Implements user features.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Define routes
-// router.post('/users', userController.createUser);
-// router.get('/users/:userId', userController.getUser);
+router.post('/users', userController.createUser);
+router.get('/users/:userId', userController.getUser);
 router.get('/users', userController.getAllUsers);
-// router.put('/users/:userId', userController.updateUser);
-// router.delete('/users/:userId', userController.deleteUser);
+router.put('/users/:userId', userController.updateUser);
+router.delete('/users/:userId', userController.deleteUser);
 
 module.exports = router;
